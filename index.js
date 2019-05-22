ab82a01a8d0f156dbd9a6412b1da39011a4222a1
Tobiasz18 <tobiaszpost@gmail.com>
2019-05-23 01:36:20 +0200

    comments

--- index.js
+++ index.js
@@ -3,20 +3,27 @@ const cors = require('cors');
 const path = require('path');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const { port, mongoUrl } = require('./config');
-const routes =  require('./routes');
 
+//endpoints
+const routes =  require('./routes'); 
+
+const { port, mongoUrl } = require('./config');
+
+// create mongo connection with url from https://mlab.com/
 mongoose.connect(mongoUrl, {useNewUrlParser: true});
 
 const app = express();
 
+//serve the static files from CRA(client folder)
 app.use(express.static(path.join(__dirname, 'client/build')));
 
 app.use(cors());
 app.use(bodyParser.json())
+
+//  All endpoints with express routers middleware
 app.use('/api', routes);
 
-
+// Handles any requests that don't match the ones above
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname + '/client/build/index.html'));
 })
